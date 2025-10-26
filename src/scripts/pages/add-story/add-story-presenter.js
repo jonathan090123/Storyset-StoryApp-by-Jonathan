ae5dcc621fd9f3f2c30562f3786d728606646f74
Jonathan Alvin <hantububu5758@gmail.com>
2025-10-27 01:41:44 +0700

    fix logo marker saat add

--- src/scripts/pages/add-story/add-story-presenter.js
+++ src/scripts/pages/add-story/add-story-presenter.js
@@ -49,7 +49,17 @@ export default class AddStoryPresenter {
             this.map.removeLayer(this.selectedMarker);
         }
 
-        this.selectedMarker = L.marker([lat, lng]).addTo(this.map);
+        // Create marker with explicit icon URLs so the image loads reliably
+        const icon = L.icon({
+            iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon.png',
+            shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png',
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34],
+            shadowSize: [41, 41]
+        });
+
+        this.selectedMarker = L.marker([lat, lng], { icon }).addTo(this.map);
         this.selectedMarker.bindPopup('Lokasi yang dipilih').openPopup();
     }
 
